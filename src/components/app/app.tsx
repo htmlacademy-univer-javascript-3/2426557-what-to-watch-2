0da3d808e2605fb89956a7bc57acd3035eadc5af
Lidiya Morshchinina <lmorshchinka@gmail.com>
2023-09-25 22:50:24 +0300

    module3-task1: add base router, page not found, private route, enums

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../../pages/main/main';
+import MyList from '../../pages/my-list/my-list';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
+import AddReview from '../../pages/add-review/add-review';
+import SignIn from '../../pages/sign-in/sign-in';
+import MoviePage from '../../pages/movie-page/movie-page';
+import Player from '../../pages/player/player';
+import { AppRoute } from '../../enums/AppRoute';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   filmName: string;
@@ -12,6 +22,43 @@ export default function App({
   releaseDate,
 }: AppProps): React.JSX.Element {
   return (
-    <MainPage filmName={filmName} genre={genre} releaseDate={releaseDate} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main}>
+          <Route
+            index
+            element={
+              <MainPage
+                filmName={filmName}
+                genre={genre}
+                releaseDate={releaseDate}
+              />
+            }
+          />
+          <Route path={AppRoute.Login} element={<SignIn />} />
+          <Route
+            path={AppRoute.MyList}
+            element={
+              <PrivateRoute>
+                <MyList />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.Films}>
+            <Route path=":id" element={<MoviePage />} />
+            <Route
+              path={AppRoute.Review}
+              element={
+                <PrivateRoute>
+                  <AddReview />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+          <Route path={AppRoute.Player} element={<Player />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/enums/AppRoute.ts
@@ -0,0 +1,8 @@
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  MyList = '/mylist',
+  Films = '/films',
+  Review = ':id/review',
+  Player = '/player/:id'
+}
