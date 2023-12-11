6976c923d2c353dfd8a92e573e1fa19adb5ade7c
Lidiya Morshchinina <lmorshchinka@gmail.com>
2023-12-11 20:14:17 +0300

    module8-task1: add rating hook

--- src/components/overview/overview.tsx
+++ src/components/overview/overview.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FilmInfoProps } from '../../types/film-types.ts';
+import { useFilmRating } from '../../hooks/film.ts';
 
 type OverviewProps = {
   film: FilmInfoProps;
 };
-export default function Overview({ film }: OverviewProps): React.JSX.Element {
+function Overview({ film }: OverviewProps): React.JSX.Element {
+  const filmRating = useFilmRating(film.rating);
+
   return (
     <>
       <div className="film-rating">
         <div className="film-rating__score">{film.rating}</div>
         <p className="film-rating__meta">
-          <span className="film-rating__level">Very good</span>
-          <span className="film-rating__count">240 ratings</span>
+          <span className="film-rating__level">{filmRating}</span>
+          <span className="film-rating__count">{film.scoresCount} ratings</span>
         </p>
       </div>
       <div className="film-card__text">
@@ -26,3 +29,6 @@ export default function Overview({ film }: OverviewProps): React.JSX.Element {
     </>
   );
 }
+const OverviewMemo = React.memo(Overview);
+
+export default OverviewMemo;
